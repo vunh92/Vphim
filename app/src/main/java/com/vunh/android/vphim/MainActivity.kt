6958e0b38afb8eee06776b2c442ec06feee3a18c
Vu NguyenH <vu.nguyenh@s3corp.com.vn>
2026-04-07 16:01:29 +0700

    update image icon

--- app/src/main/java/com/vunh/android/vphim/MainActivity.kt
+++ app/src/main/java/com/vunh/android/vphim/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -18,6 +19,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import com.vunh.android.vphim.presentation.ui.navigation.Destination
 import com.vunh.android.vphim.presentation.ui.screen.anime.AnimeMoviesScreen
 import com.vunh.android.vphim.presentation.ui.screen.favorite.FavoriteScreen
@@ -51,7 +53,8 @@ fun VphimApp() {
                     icon = {
                         Icon(
                             painter = painterResource(destination.icon),
-                            contentDescription = destination.label
+                            contentDescription = destination.label,
+                            modifier = Modifier.size(24.dp)
                         )
                     },
                     label = { Text(destination.label) },
--- app/src/main/java/com/vunh/android/vphim/presentation/ui/navigation/Destination.kt
+++ app/src/main/java/com/vunh/android/vphim/presentation/ui/navigation/Destination.kt
@@ -7,8 +7,8 @@ enum class Destination(
     val icon: Int,
 ) {
     HOME("Trang chủ", R.drawable.ic_home),
-    SERIES("Phim Bộ", R.drawable.ic_home),
-    SINGLE("Phim Lẻ", R.drawable.ic_home),
-    ANIME("Hoạt Hình", R.drawable.ic_home),
+    SERIES("Phim Bộ", R.drawable.ic_series),
+    SINGLE("Phim Lẻ", R.drawable.ic_single),
+    ANIME("Hoạt Hình", R.drawable.ic_anime),
     FAVORITES("Yêu thích", R.drawable.ic_favorite),
 }
